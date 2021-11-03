f3d23721f87eb6d15779cda33cc943cd5802bd0e
Camila <camilaaparicioo@hotmail.com>
2021-11-02 23:42:35 -0400

    some js attempts

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const burger = document.querySelector('label')
+burger.addEventListener('click', () => {
+    console.log('CLICKED!')
+    alert('burger nav menu coming soon!')
+});
+
+
+const received = document.querySelector('submit')
+
+received.addEventListener('click', () => {
+    console.log("submited!!")
+    alert('thanks! we will get back to you tmrw.')
+});
+
+
+
+// const hamburger = document.getElementsByClassName('burger')
+// const nav = document.getElementsByClassName('nav')
+
+// hamburger.addEventListener('click', () => {
+//     nav.classlist.toggle('show');
+// })
+
+
+
+// likes button
+// const heart = document.querySelector('heart')
+
+
+
